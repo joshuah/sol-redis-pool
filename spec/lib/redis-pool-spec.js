21334f1388cc0034a9ea89f001ba47ebd999f011
Julien Kernec'h <julien.kernech@outlook.com>
2015-11-13 17:09:46 +0100

    Add beforeAll to setup redisPool

--- spec/lib/redis-pool-spec.js
+++ spec/lib/redis-pool-spec.js
@@ -1,3 +1,12 @@
 var config = require('../config.json');
 var RedisPool = require('../../index');
 
+
+var redisPool;
+
+beforeAll(function() {
+  redisPool = new RedisPool(config.redis, {
+    max: 10,
+    min: 2
+  });
+});
